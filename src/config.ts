dd6b6052d6bf4675a72b47524c8d5cdca57ba907
Zydr <z.ydr1992@gmail.com>
2026-03-07 20:34:44 +0800

    更换了个人头像

--- src/config.ts
+++ src/config.ts
@@ -318,7 +318,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.png", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Zydr",
 	bio: "锐意学习中。。。",
 	typewriter: {
